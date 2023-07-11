7b9831bdddabf580152d59c455a25651b22741e4
Guido Mallamace <guidomallamace97@gmail.com>
2023-07-11 20:18:21 +0200

    Rifiniture

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,33 +9,39 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   isLoading: boolean = true;
 
-  constructor() { }
+  constructor() {
+
+   }
 
   ngOnInit() {
+
     setTimeout(() => {
       this.isLoading = false;
     }, 2000);
   }
+apri(event:MouseEvent){
+  const faq= event.target! as HTMLElement
+          faq.classList.toggle('open');
+  
+          console.log('ciao')
+  
+          // Cambio icona
+          const icon = faq.querySelector('.faq__icon i');
+         /* if(icon!.className === 'uil uil-plus') {
+          console.log('ciao2')
+            icon!.className = "uil uil-minus";
+          } else {
+              icon!.className = "uil uil-plus";
+  
+          } */
+          icon?.classList.toggle('uil-plus');
+          icon?.classList.toggle('uil-minus');
+}
 }
 
 
 // Mostra e nascondi domadne frequenti
 
-const faqs = document.querySelectorAll('.faq');
 
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle('open');
-
-        // Cambio icona
-        const icon = faq.querySelector('.faq__icon i');
-        if(icon!.className === 'uil uil-plus') {
-            icon!.className = "uil uil-minus";
-        } else {
-            icon!.className = "uil uil-plus";
-
-        }
-    })
-})
 
 
