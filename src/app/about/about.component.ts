5bb9d8fb5b69a3daae4a4df4b48ff483b1f691ad
Guido Mallamace <guidomallamace97@gmail.com>
2023-07-18 12:14:43 +0200

    Modifiche sezione About

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,13 +9,28 @@ export class AboutComponent {
   isLogged: boolean = false;
 
   ngOnInit() {
-   this.checkLog()
+    this.checkLog();
   }
- 
+
   checkLog() {
-   let user = localStorage.getItem('user');
-   if (user) {
-   this.isLogged = true;
-   }
-   }
+    let user = localStorage.getItem('user');
+    if (user) {
+      this.isLogged = true;
+    }
+  }
+
+  email: string = '';
+
+  onSubmit() {
+    if (this.validateEmail(this.email)) {
+      localStorage.setItem('email', this.email);
+      alert('Email inviata correttamente!');
+    } else {
+      alert('Inserisci un indirizzo email valido!');
+    }
+  }
+
+  validateEmail(email: string) {
+    return email.includes('@');
+  }
 }
